be9ed111604b7a8c8ad75c60976ac9e22d762855
Hubert Thieriot <hubert@energyandcleanair.org>
2020-12-29 11:35:22 +0800

    -new color scheme -filing dates

--- analysis.R
+++ analysis.R
@@ -12,10 +12,11 @@ m <- rcrea::measurements(source="mee",
                            rcrea::PM10,
                            rcrea::NO2,
                            rcrea::SO2),
-                         date_from="2018-01-01",
+                         date_from="2016-01-01",
                          date_to="2020-12-31",
                          with_metadata=T) %>%
-  spread("poll","value")
+  spread("poll","value") %>%
+  mutate(date=lubridate::date(date))
 
 # Add province name
 m <- m %>% left_join(read.csv("data/gadm1.csv") %>% mutate(gadm1_id=tolower(GID_1)) %>%
@@ -24,18 +25,20 @@ m <- m %>% left_join(read.csv("data/gadm1.csv") %>% mutate(gadm1_id=tolower(GID_
 # Add AQI
 m.aqi <- m %>% utils.add_aqi() %>% filter(!is.na(province_en))
 saveRDS(m.aqi,  file.path("results","m.aqi.RDS"))
-write.csv(m.aqi, file.path("results","m.aqi.csv"), row.names=F)
+
+# Fill missing dates with NA to ensure comparability across years
+m.aqi <- m.aqi %>% utils.fill_city_dates(min_date=min(m$date))
 
 
 # Daily heatmap ----------------------------------------------------------------
-(plt <- ggplot(m.aqi, aes(x=date, y=location_name, fill=aqi_cat_en)) +
+(plt <- ggplot(m.aqi %>% filter(date>="2018-01-01"), aes(x=date, y=location_name, fill=aqi_cat_en)) +
   geom_tile() +
    facet_wrap(~province_en, scales="free_y") +
   theme(axis.text.y=element_blank(),
         axis.ticks.y=element_blank()) +
-  scale_x_datetime(date_labels="%Y", date_breaks = "1 year") +
-  scale_fill_brewer(palette="RdYlGn", direction=-1, name=NULL) +
-  # scale_fill_manual(values=c("green","yellow","orange","red","purple","brown"), name=NULL) +
+  scale_x_date(date_labels="%Y", date_breaks = "1 year") +
+  # scale_fill_brewer(palette="RdYlGn", direction=-1, name=NULL) +
+  scale_fill_manual(values=c("#50C878","#F8E473","orange","red","purple","brown"), name=NULL) +
   labs(y=NULL, x=NULL,
        title="AQI Categoty in Chinese cities",
        subtitle="Each row corresponds to a city",
@@ -45,7 +48,7 @@ ggsave(file.path("results","plots","heatmap.png"), width=12, height=8)
 
 
 # Responsible pollutant per category ------------------------------------------
-(plt <- ggplot(m.aqi) +
+(plt <- ggplot(m.aqi %>% filter(date>="2018-01-01")) +
    geom_bar(stat="count", aes(x=aqi_argmax,
                               group=aqi_cat_en,
                               y=..prop..,
@@ -66,7 +69,7 @@ ggsave(file.path("results","plots","aqi_pollutants.png"), width=8, height=5)
 
 
 # Responsible pollutant per province ------------------------------------------
-(plt <- ggplot(m.aqi %>% filter(aqi_cat >= 5)) +
+(plt <- ggplot(m.aqi %>% filter(aqi_cat>=5, date>="2018-01-01")) +
    geom_bar(stat="count", aes(x=aqi_argmax,
                               group=province_en,
                               y=..prop..,
@@ -84,3 +87,32 @@ ggsave(file.path("results","plots","aqi_pollutants.png"), width=8, height=5)
         caption="Source: CREA based on MEE"))
 
 ggsave(file.path("results","plots","aqi_pollutants_province.png"), width=12, height=8)
+
+
+# Number of days with AQI>200 per province ------------------------------------------
+(plt <- ggplot(m.aqi %>%
+                 group_by(province_en, aqi_cat, aqi_cat_en, year=lubridate::year(date)) %>%
+                 summarise(count=n()) %>%
+                 mutate(aqi_cat_en=factor(aqi_cat_en, levels=rev(levels(.$aqi_cat_en))))
+                 ) +
+   geom_area(stat="identity", aes(x=year,
+                               y=count,
+                               fill=factor(aqi_cat_en))) +
+   # guides(fill = guide_legend(reverse=TRUE)) +
+   # geom_area(stat="count", aes(x=year,
+   #                            group=province_en,
+   #                            y=..prop..,
+   #                            fill=factor(..x..)),
+   #          show.legend = F) +
+   facet_wrap(~province_en, scales="free_y") +
+   theme(axis.text.y=element_blank(),
+         axis.ticks.y=element_blank()) +
+   scale_x_discrete(drop=FALSE) +
+   # scale_y_continuous(labels=scales::percent) +
+   scale_fill_brewer(palette="RdYlGn", direction=1, name=NULL, na.value="grey") +
+   rcrea::theme_crea() +
+   labs(x=NULL,y=NULL,
+        title="Air quality levels in Chinese regions",
+        caption="Source: CREA based on MEE"))
+
+ggsave(file.path("results","plots","aqi_ts_province.png"), width=12, height=8)
--- utils.R
+++ utils.R
@@ -3,12 +3,11 @@ utils.add_aqi <- function(df){
   # Using this: https://www.mee.gov.cn/ywgz/fgbz/bz/bzwb/jcffbz/201203/W020120410332725219541.pdf
   df %>% mutate(
     aqi_pm25=replace_na(approx(x=c(0, 35, 75, 115, 150, 250, 350, 500, Inf),
-                    y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
-                    xout=pm25)$y,0),
-
+                               y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
+                               xout=pm25)$y,0),
     aqi_pm10=replace_na(approx(x=c(0, 50, 150, 250, 350, 420, 500, 600, Inf),
-                    y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
-                    xout=pm10)$y,0),
+                               y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
+                               xout=pm10)$y,0),
     aqi_no2=replace_na(approx(x=c(0, 40, 80, 180, 280, 565, 750, 940, Inf),
                               y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
                               xout=no2)$y,0),
@@ -17,8 +16,8 @@ utils.add_aqi <- function(df){
                               xout=so2)$y,0),
     aqi=pmax(aqi_pm25, aqi_pm10, aqi_no2, aqi_so2, na.rm=T),
     aqi_cat=as.integer(cut(aqi,
-                breaks=c(0,50,100,150,200,300,400,500,Inf),
-                labels=c(1,2,3,4,5,6,6,6)))
+                           breaks=c(0,50,100,150,200,300,400,500,Inf),
+                           labels=c(1,2,3,4,5,6,6,6)))
   ) %>%
     mutate(aqi_argmax=factor(max.col(.[c("aqi_pm25","aqi_pm10","aqi_no2", "aqi_so2")]),
                              seq(1,4),
@@ -27,4 +26,18 @@ utils.add_aqi <- function(df){
     left_join(tibble(aqi_cat=seq(1,6),
                      aqi_cat_zh=factor(seq(1,6), seq(1,6), labels=c("优","良", "轻度污染", "中度污染", "重度污染", "严重污染"), ordered=T),
                      aqi_cat_en=factor(seq(1,6), seq(1,6), labels=c("Excellent", "Good", "Mildly polluted", "Moderately polluted", "Heavily polluted", "Severely polluted"), ordered=T)))
+}
+
+
+utils.fill_city_dates <- function(m, min_date="2018-01-01"){
+  # To ensure comparability across years,
+  # we fill empty cityxdate combinations with NA
+
+  date <- seq(lubridate::date(min_date), lubridate::today(), by="day")
+  m %>% right_join(
+    m %>% distinct(
+      location_id, location_name, province_en, gadm1_id, process_id
+    ) %>%
+      tidyr::crossing(date)
+  )
 }
