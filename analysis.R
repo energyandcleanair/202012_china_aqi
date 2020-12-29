1e6a730f84cd13c30d1b0dac5c53b6943a5a82b5
Hubert Thieriot <hubert@energyandcleanair.org>
2020-12-29 09:37:56 +0800

    -with updated standard

--- analysis.R
+++ analysis.R
@@ -1,31 +1,21 @@
 remotes::install_github('energyandcleanair/rcrea')
 library(rcrea)
 library(dplyr)
+library(tidyr)
 
 
-# We join in db to reduce amount to download and
-# thereby accelerate download
+
 m <- rcrea::measurements(source="mee",
                          process_id = "city_day_mad",
-                         poll=c(rcrea::PM10),
+                         poll=c(
+                           rcrea::PM25,
+                           rcrea::PM10,
+                           rcrea::NO2,
+                           rcrea::SO2),
                          date_from="2018-01-01",
                          date_to="2020-12-31",
-                         collect=F,
                          with_metadata=T) %>%
-  rename(pm10=value) %>%
-  select(-c(id, poll, geometry)) %>%
-  dplyr::full_join(
-    rcrea::measurements(source="mee",
-                        process_id = "city_day_mad",
-                        poll=c(rcrea::PM25),
-                        date_from="2018-01-01",
-                        date_to="2020-12-31",
-                        collect=F,
-                        with_metadata=F) %>%
-      select(process_id, date, location_id, unit, pm25=value)
-  ) %>%
-  collect() %>%
-  ungroup()
+  spread("poll","value")
 
 # Add province name
 m <- m %>% left_join(read.csv("data/gadm1.csv") %>% mutate(gadm1_id=tolower(GID_1)) %>%
@@ -38,16 +28,38 @@ write.csv(m.aqi, file.path("results","m.aqi.csv"), row.names=F)
 
 
 # Daily heatmap ----------------------------------------------------------------
-(plt <- ggplot(m.aqi, aes(x=date, y=location_name, fill=aqi_cat)) +
+(plt <- ggplot(m.aqi, aes(x=date, y=location_name, fill=aqi_cat_en)) +
   geom_tile() +
    facet_wrap(~province_en, scales="free_y") +
   theme(axis.text.y=element_blank(),
         axis.ticks.y=element_blank()) +
-   scale_x_datetime(date_labels="%Y") +
+  scale_x_datetime(date_labels="%Y", date_breaks = "1 year") +
   scale_fill_brewer(palette="RdYlGn", direction=-1, name=NULL) +
+  # scale_fill_manual(values=c("green","yellow","orange","red","purple","brown"), name=NULL) +
   labs(y=NULL, x=NULL,
        title="AQI Categoty in Chinese cities",
        subtitle="Each row corresponds to a city",
        caption="Source: CREA based on MEE"))
 
 ggsave(file.path("results","plots","heatmap.png"), width=12, height=8)
+
+
+# Responsible pollutant ---------------------------------------------------
+(plt <- ggplot(m.aqi) +
+   geom_bar(stat="count", aes(x=factor(aqi_argmax),
+                              group=aqi_cat_en,
+                              y=..prop..,
+                              fill=factor(..x..)),
+            show.legend = F) +
+   facet_wrap(~aqi_cat_en, scales="free_x") +
+   theme(axis.text.y=element_blank(),
+         axis.ticks.y=element_blank()) +
+   scale_x_discrete(drop=FALSE) +
+   scale_y_continuous(labels=scales::percent) +
+   rcrea::theme_crea() +
+   labs(x=NULL,y=NULL,
+        title="Pollutant determining the daily AQI",
+        subtitle="2018-2020",
+        caption="Source: CREA based on MEE"))
+
+ggsave(file.path("results","plots","aqi_pollutants.png"), width=8, height=5)
--- utils.R
+++ utils.R
@@ -1,19 +1,27 @@
 utils.add_aqi <- function(df){
-  # Capped at 300
-  #TODO confirm definition
+  # Capping at 500
+  # Using this: https://www.mee.gov.cn/ywgz/fgbz/bz/bzwb/jcffbz/201203/W020120410332725219541.pdf
   df %>% mutate(
-    aqi_pm25=replace_na(approx(x=c(0, 35, 75, 115, 150, 250, Inf),
-                    y=c(0, 50, 100, 150, 200, 300, 300),
+    aqi_pm25=replace_na(approx(x=c(0, 35, 75, 115, 150, 250, 350, 500, Inf),
+                    y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
                     xout=pm25)$y,0),
 
-    aqi_pm10=replace_na(approx(x=c(0, 50, 150, 250, 350, 420, Inf),
-                    y=c(0, 50, 100, 150, 200, 300, 300),
+    aqi_pm10=replace_na(approx(x=c(0, 50, 150, 250, 350, 420, 500, 600, Inf),
+                    y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
                     xout=pm10)$y,0),
-    aqi=pmax(aqi_pm25, aqi_pm10, na.rm=T),
-    aqi_argmax=ifelse(aqi_pm25>aqi_pm10 | is.na(aqi),"pm25","pm10"),
-    aqi_cat=cut(aqi,
-                breaks=c(0,50,100,150,200,300,Inf),
-                labels=c("Excellent","Good","Slightly Polluted","Lightly Polluted",
-                         "Moderately Polluted", "Heavily Polluted"))
-  )
+    aqi_no2=replace_na(approx(x=c(0, 40, 80, 180, 280, 565, 750, 940, Inf),
+                              y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
+                              xout=no2)$y,0),
+    aqi_so2=replace_na(approx(x=c(0, 50, 150, 475, 800, 1600, 2100, 2620, Inf),
+                              y=c(0, 50, 100, 150, 200, 300, 400, 500, 500),
+                              xout=so2)$y,0),
+    aqi=pmax(aqi_pm25, aqi_pm10, aqi_no2, aqi_so2, na.rm=T),
+    aqi_cat=as.integer(cut(aqi,
+                breaks=c(0,50,100,150,200,300,400,500,Inf),
+                labels=c(1,2,3,4,5,6,6,6)))
+  ) %>%
+    mutate(aqi_argmax=c("pm25","pm10","no2","so2")[max.col(.[c("aqi_pm25","aqi_pm10","aqi_no2", "aqi_so2")])]) %>%
+    left_join(tibble(aqi_cat=seq(1,6),
+                     aqi_cat_zh=factor(seq(1,6), seq(1,6), labels=c("优","良", "轻度污染", "中度污染", "重度污染", "严重污染"), ordered=T),
+                     aqi_cat_en=factor(seq(1,6), seq(1,6), labels=c("Excellent", "Good", "Mildly polluted", "Moderately polluted", "Heavily polluted", "Severely polluted"), ordered=T)))
 }
